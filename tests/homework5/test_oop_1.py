01618993f8b0a59ed21eebad5308b6bfa78447c1
kuweg <thekuweg@gmail.com>
2021-11-24 20:19:21 +0300

    complete home task 5

---
+++ tests/homework5/test_oop_1.py
@@ -0,0 +1,39 @@
+import datetime
+
+from homework5.oop_1 import Student, Teacher
+
+
+student = Student('Nikita', 'Zaitsev')
+teacher = Teacher('Ivan', 'Ivanov')
+expired_homework = teacher.create_homework('Learn functions', 0)
+weekly_homework = teacher.create_homework('Lecture 5', 7)
+
+
+def test_student_name():
+    assert (student.first_name == 'Nikita' and
+            student.last_name == 'Zaitsev')
+
+
+def test_teacher_name():
+    assert (teacher.first_name == 'Ivan' and
+            teacher.last_name == 'Ivanov')
+
+
+def test_expired_homework_deadline():
+    assert expired_homework.deadline == datetime.timedelta(0)
+
+
+def test_weekly_homework_deadline():
+    assert weekly_homework.deadline == datetime.timedelta(7)
+
+
+def test_homework_task():
+    assert weekly_homework.text == "Lecture 5"
+
+
+def test_expired_homework_message():
+    assert student.do_homework(expired_homework) is None
+
+
+def test_homework_deadline_positive():
+    assert (student.do_homework(weekly_homework) == weekly_homework)
